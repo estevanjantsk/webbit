16da3992fcfdcb02eb00c83bc887ec6af9af1282
Estevan Jantsk <estevan.jantsk@gmail.com>
2020-02-09 22:14:05 +0200

    Adding Submission - scaffold

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_09_174713) do
+ActiveRecord::Schema.define(version: 2020_02_09_193940) do
+
+  create_table "submissions", force: :cascade do |t|
+    t.string "title"
+    t.text "body"
+    t.string "url"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
