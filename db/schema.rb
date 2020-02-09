2b8339231b0f6a5dd1fd23fdf1d3288696eaf23f
Estevan Jantsk <estevan.jantsk@gmail.com>
2020-02-09 23:40:10 +0200

    Adding relationship to submissions and user

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_09_205253) do
+ActiveRecord::Schema.define(version: 2020_02_09_210619) do
 
   create_table "submissions", force: :cascade do |t|
     t.string "title"
@@ -20,6 +20,8 @@ ActiveRecord::Schema.define(version: 2020_02_09_205253) do
     t.datetime "updated_at", precision: 6, null: false
     t.string "submission_image"
     t.string "submission_video"
+    t.integer "user_id"
+    t.index ["user_id"], name: "index_submissions_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -34,4 +36,5 @@ ActiveRecord::Schema.define(version: 2020_02_09_205253) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "submissions", "users"
 end
