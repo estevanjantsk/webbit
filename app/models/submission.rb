dbb0a9e0ade8c4b12d10077f085e0d3c60487c2d
Estevan Jantsk <estevan.jantsk@gmail.com>
2020-02-10 21:48:46 +0200

    Active Record Validations

--- app/models/submission.rb
+++ app/models/submission.rb
@@ -3,4 +3,9 @@ class Submission < ApplicationRecord
   mount_uploader :submission_video, SubmissionVideoUploader
 
   belongs_to :user
+
+  # validations
+  validates :title, presence: true
+  validates :body,  length: { maximum: 8000 }
+  validates :url, url: { allow_blank: true }
 end
