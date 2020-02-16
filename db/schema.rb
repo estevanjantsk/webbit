54fef33b408852d5d7f690f8ac31570b3073f39d
Estevan Jantsk <estevan.jantsk@gmail.com>
2020-02-16 13:07:53 +0200

    Create the comment model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_15_102450) do
+ActiveRecord::Schema.define(version: 2020_02_16_110427) do
+
+  create_table "comments", force: :cascade do |t|
+    t.text "reply"
+    t.integer "submission_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["submission_id"], name: "index_comments_on_submission_id"
+  end
 
   create_table "communities", force: :cascade do |t|
     t.string "name"
@@ -52,6 +60,7 @@ ActiveRecord::Schema.define(version: 2020_02_15_102450) do
     t.index ["username"], name: "index_users_on_username", unique: true
   end
 
+  add_foreign_key "comments", "submissions"
   add_foreign_key "communities", "users"
   add_foreign_key "submissions", "communities"
   add_foreign_key "submissions", "users"
