7f02d5069abf8f83fe78866a91751c2b15e235a3
Estevan Jantsk <estevan.jantsk@gmail.com>
2020-02-24 20:26:41 +0200

    rails action_text:install

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -20,3 +20,6 @@ import "stylesheets/application";
 //
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
+
+require("trix")
+require("@rails/actiontext")
