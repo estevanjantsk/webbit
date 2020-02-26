eee0e93c0ef6b8ed76dc92f338f17c5a5b9337af
Estevan Jantsk <estevan.jantsk@gmail.com>
2020-02-26 23:58:36 +0200

    BONUS: Stubbing out add spaces

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,4 +4,14 @@ module ApplicationHelper
     title ||= name.underscore.humanize
     inline_svg_tag filename, aria: true, nocomment: true, title: title, class: styles
   end
+
+  def inject_feed_ad(index)
+    if((index + 1) % 3 == 0)
+      render "feed_ad"
+    end
+  end
+
+  def active_subscriber?
+    user_signed_in? && current_user.subscribed?
+  end
 end
